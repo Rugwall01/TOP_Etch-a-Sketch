2137d4dddf215ed5c99b3375ccbe82f54126143a
rugwall01 <161780368+Rugwall01@users.noreply.github.com>
2024-11-18 11:27:35 -0800

    nested loop to create grid of boxes inside loop to create grid of columns/rows

--- script.js
+++ script.js
@@ -2,15 +2,21 @@
 const body = document.querySelector("body");
 
 
-function createElementsWithClass(n, className) {
-    for(let i = 0; i < n; i++){
-        const element = document.createElement('div');
+function createElementsWithClass(n1, n2, className1, className2) {
+    for(let i = 0; i < n1; i++){
+        const elementY = document.createElement('div');
+        elementY.classList.add(className1 +  `${(i + 1)}`);
+        
+        for(let j = 0; j < n2; j++){
+            const elementX = document.createElement('div');
 
-        element.classList.add(className);
+            elementX.classList.add(className2);
 
-        body.appendChild(element);
+            elementY.appendChild(elementX);
+        }
+        body.appendChild(elementY);
     }
 }
 
 
-createElementsWithClass(16, 'grid');
+createElementsWithClass(16, 16, 'grid-y', 'grid-x');
